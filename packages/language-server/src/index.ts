623e298b728d99b8a850e92acc4912551532eec7
so1ve <i@mk1.io>
2024-02-27 10:03:42 +0800

    feat: upgrade to volar ~2.1.0

--- packages/language-server/src/index.ts
+++ packages/language-server/src/index.ts
@@ -2,7 +2,8 @@ import type { Diagnostic, VirtualCode } from "@volar/language-server/node";
 import {
 	createConnection,
 	createServer,
-	createTypeScriptProjectProvider,
+	createTypeScriptProjectProviderFactory,
+	loadTsdkByPath,
 } from "@volar/language-server/node";
 import { create as createCssService } from "volar-service-css";
 import { create as createEmmetService } from "volar-service-emmet";
@@ -17,55 +18,67 @@ const server = createServer(connection);
 
 connection.listen();
 
-connection.onInitialize((params) =>
-	server.initialize(params, createTypeScriptProjectProvider, {
-		getLanguagePlugins() {
-			return [html1LanguagePlugin];
-		},
-		getServicePlugins() {
-			return [
-				createHtmlService(),
-				createCssService(),
-				createEmmetService(),
-				createTypeScriptService(server.modules.typescript),
-				{
-					create(context) {
-						return {
-							provideDiagnostics(document) {
-								const virtualCode = context.documents.getVirtualCodeByUri(
-									document.uri,
-								)[0] as VirtualCode | Html1Code | undefined;
-								if (!virtualCode || !("htmlDocument" in virtualCode)) {
-									return;
-								}
-								const styleNodes = virtualCode.htmlDocument.roots.filter(
-									(root) => root.tag === "style",
-								);
-								if (styleNodes.length <= 1) {
-									return;
-								}
-								const errors: Diagnostic[] = [];
-								for (let i = 1; i < styleNodes.length; i++) {
-									errors.push({
-										severity: 2,
-										range: {
-											start: document.positionAt(styleNodes[i].start),
-											end: document.positionAt(styleNodes[i].end),
-										},
-										source: "html1",
-										message: "Only one style tag is allowed.",
-									});
-								}
+connection.onInitialize((params) => {
+	const tsdk = loadTsdkByPath(
+		params.initializationOptions.typescript.tsdk,
+		params.locale,
+	);
 
-								return errors;
-							},
-						};
+	return server.initialize(
+		params,
+		createTypeScriptProjectProviderFactory(
+			tsdk.typescript,
+			tsdk.diagnosticMessages,
+		),
+		{
+			getLanguagePlugins() {
+				return [html1LanguagePlugin];
+			},
+			getServicePlugins() {
+				return [
+					createHtmlService(),
+					createCssService(),
+					createEmmetService(),
+					createTypeScriptService(tsdk.typescript),
+					{
+						create(context) {
+							return {
+								provideDiagnostics(document) {
+									const virtualCode = context.documents.getVirtualCodeByUri(
+										document.uri,
+									)[0] as VirtualCode | Html1Code | undefined;
+									if (!virtualCode || !("htmlDocument" in virtualCode)) {
+										return;
+									}
+									const styleNodes = virtualCode.htmlDocument.roots.filter(
+										(root) => root.tag === "style",
+									);
+									if (styleNodes.length <= 1) {
+										return;
+									}
+									const errors: Diagnostic[] = [];
+									for (let i = 1; i < styleNodes.length; i++) {
+										errors.push({
+											severity: 2,
+											range: {
+												start: document.positionAt(styleNodes[i].start),
+												end: document.positionAt(styleNodes[i].end),
+											},
+											source: "html1",
+											message: "Only one style tag is allowed.",
+										});
+									}
+
+									return errors;
+								},
+							};
+						},
 					},
-				},
-			];
+				];
+			},
 		},
-	}),
-);
+	);
+});
 
 connection.onInitialized(server.initialized);
 connection.onShutdown(server.shutdown);
